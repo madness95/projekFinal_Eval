fc45233729e4dcc5d7803e99eb96db800a51ae01
khulqi <khulqimuhamad@gmail.com>
2019-05-31 09:12:45 +0700

    berhasil tampil tabel dr database

--- src/main/java/com/eval/controllers/MainControllers.java
+++ src/main/java/com/eval/controllers/MainControllers.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -23,17 +24,40 @@ public class MainControllers {
     @Autowired
     private RoleRepositories roleRepositories;
     
-    @GetMapping("/role/all")
+    @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("dataRole", roleRepositories.findAll());
-        return "roleIndex";
+        return "index";
     }
     
-//    @PostMapping("/addDataRole")
-//    public String addData (@Valid Role role){
-//        roleRepositories.save(role);
-////        model.addAttribute("dataActor", filmActorService.findAllActor());
-////        return "index";
-//        return "redirect:/indexRole";
-//    }
+    @PostMapping("/addDataRole")
+    public String addData (@Valid Role role){
+        roleRepositories.save(role);
+//        model.addAttribute("dataActor", filmActorService.findAllActor());
+//        return "index";
+        return "redirect:/";
+    }
+    
+    @GetMapping("/update")
+    public String updateActor(@Valid Role role) {
+//        if (result.hasErrors()) {
+//            actor.setActorId(id);
+//            return "updateActor";
+//        }
+        roleRepositories.save(role);
+//        model.addAttribute("dataActor", filmActorService.findAllActor());
+//        return "index";
+        return "redirect:/";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String deleteActor(@PathVariable("id") Integer id, Model model) {
+//        Short ida = id;
+//        int id_actor= ida.intValue();
+        Role role = roleRepositories.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid role Id:" + id));
+        roleRepositories.delete(role);
+        model.addAttribute("role", roleRepositories.findAll());
+        return "index";
+    }
 }
