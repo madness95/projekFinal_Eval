4c890333d25200a7644b8f63d1010603c456e67a
khulqi <khulqimuhamad@gmail.com>
2019-06-13 21:45:06 +0700

    sql

--- eval (8).sql
+++ eval (8).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2019 at 03:46 AM
+-- Generation Time: Jun 13, 2019 at 04:42 PM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.3.5
 
@@ -33,7 +33,8 @@ CREATE TABLE `assignment` (
   `instruction` varchar(200) DEFAULT NULL,
   `deadline` date NOT NULL,
   `date` date NOT NULL,
-  `employee` int(10) NOT NULL,
+  `uploadby` int(10) NOT NULL,
+  `file` varchar(50) NOT NULL,
   `isdelete` enum('true','false') NOT NULL DEFAULT 'false',
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -41,15 +42,25 @@ CREATE TABLE `assignment` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `auth_user`
+-- Table structure for table `authuser`
 --
 
-CREATE TABLE `auth_user` (
-  `emp_id` int(10) NOT NULL,
-  `role_id` int(10) NOT NULL,
-  `isdelete` enum('true','false') NOT NULL DEFAULT 'false',
+CREATE TABLE `authuser` (
+  `id` int(10) NOT NULL,
+  `empid` int(20) NOT NULL,
+  `roleid` int(10) NOT NULL,
+  `isdelete` enum('true','false') NOT NULL,
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `authuser`
+--
+
+INSERT INTO `authuser` (`id`, `empid`, `roleid`, `isdelete`, `last_update`) VALUES
+(4, 13, 8, 'false', '2019-06-12 04:46:16'),
+(5, 14, 32, 'false', '2019-06-12 04:46:24'),
+(6, 15, 7, 'false', '2019-06-12 04:46:31');
 
 -- --------------------------------------------------------
 
@@ -72,8 +83,9 @@ CREATE TABLE `batch` (
 --
 
 INSERT INTO `batch` (`id`, `classid`, `batch`, `period`, `trainer`, `isdelete`, `lastupdate`) VALUES
-(33, 1, '25', '2018-12-30', 2, 'false', '2019-06-09 17:07:50'),
-(34, 1, '29', '2018-12-30', 2, 'false', '2019-06-09 17:14:08');
+(56, 1, '25', '2019-01-09', 2, 'false', '2019-06-11 02:25:53'),
+(57, 2, '26', '2019-06-30', 2, 'false', '2019-06-11 04:56:40'),
+(58, 7, '29', '2019-06-02', 2, 'false', '2019-06-13 01:40:55');
 
 -- --------------------------------------------------------
 
@@ -94,7 +106,11 @@ CREATE TABLE `class` (
 INSERT INTO `class` (`id`, `name`, `isdelete`) VALUES
 (1, 'Java', 'false'),
 (2, '.Net', 'false'),
-(3, 'Cisco', 'true');
+(3, 'Cisco', 'true'),
+(4, 'php', 'true'),
+(5, 'cisco', 'false'),
+(6, 'Spring', 'false'),
+(7, 'Visual basic', 'false');
 
 -- --------------------------------------------------------
 
@@ -116,7 +132,9 @@ INSERT INTO `department` (`id`, `name`, `isdelete`) VALUES
 (1, 'ADD 1', 'false'),
 (2, 'ADD 2', 'false'),
 (3, 'MSBU', 'false'),
-(4, 'HRD', 'true');
+(4, 'HRD', 'true'),
+(5, 'IT PROG', 'true'),
+(6, 'ADD 4', 'true');
 
 -- --------------------------------------------------------
 
@@ -126,19 +144,29 @@ INSERT INTO `department` (`id`, `name`, `isdelete`) VALUES
 
 CREATE TABLE `employees` (
   `id` int(10) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
+  `firstname` varchar(20) NOT NULL,
+  `lastname` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `phone_number` varchar(12) NOT NULL,
+  `phonenumber` varchar(12) NOT NULL,
   `birthdate` date NOT NULL,
   `hiredate` date NOT NULL,
-  `job` varchar(20) NOT NULL,
-  `department` int(20) DEFAULT NULL,
+  `jobid` varchar(20) NOT NULL,
+  `departmentid` int(20) DEFAULT NULL,
   `batchclass` int(20) DEFAULT NULL,
-  `isdelete` enum('true','false') NOT NULL DEFAULT 'false',
+  `isdelete` enum('true','false') NOT NULL,
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `firstname`, `lastname`, `email`, `password`, `phonenumber`, `birthdate`, `hiredate`, `jobid`, `departmentid`, `batchclass`, `isdelete`, `last_update`) VALUES
+(13, 'Muhamad ', 'Khulqi', 'khulqimuhamad@gmail.com', '$2a$10$SDFRP3m2cj3a8H2b4OYl8O2UvoXAJhvDhq3brHlefgbVY2iYjc0Gu', '89694320475', '1997-09-07', '2019-10-08', 'IT-DEV', 1, 56, 'false', '2019-06-13 14:21:50'),
+(14, 'Oka', 'Lanang ', 'okalanang13@gmail.com', '$2a$10$suDEXHFJpXZ/dNwtTpyK/u/QP25t.kb6xvf9DKwkW9tV8i2usiLZa', '89694320475', '1998-09-10', '2019-01-02', 'IT-DEV', 1, 57, 'false', '2019-06-12 04:00:17'),
+(15, 'Rossa ', 'Hening ', 'rossahening@gmai.com', 'sarimisotokoya', '89694320475', '1985-02-09', '2019-10-10', 'IT-DEV', 1, NULL, 'false', '2019-06-12 04:02:35'),
+(16, 'Regita ', 'Rachma ', 'gitarachma85@gmail.com', 'sarimisotokoya', '89694320475', '1999-10-10', '2019-10-10', 'IT-DEV', 2, 56, 'false', '2019-06-12 07:45:05');
 
 -- --------------------------------------------------------
 
@@ -234,11 +262,12 @@ INSERT INTO `role` (`id`, `name`, `isdelete`) VALUES
 (8, 'trainer', 'false'),
 (25, 'trainer 2', 'true'),
 (26, 'HRD', 'true'),
-(27, 'user', 'true'),
+(27, 'USER', 'true'),
 (28, 'CEO', 'true'),
 (29, 'HRD', 'true'),
 (30, 'HRD', 'true'),
-(31, 'trainer 2', 'true');
+(31, 'trainer 2', 'true'),
+(32, 'ADMIN', 'false');
 
 -- --------------------------------------------------------
 
@@ -254,6 +283,40 @@ CREATE TABLE `task` (
   `mark` float NOT NULL,
   `isdelete` enum('true','false') NOT NULL DEFAULT 'false',
   `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `uplo`
+--
+
+CREATE TABLE `uplo` (
+  `id` int(11) NOT NULL,
+  `file` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `uplo`
+--
+
+INSERT INTO `uplo` (`id`, `file`) VALUES
+(1, 'images.jpg'),
+(2, 'IMG-20171010-WA0011.jpg'),
+(3, 'Department');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `validation`
+--
+
+CREATE TABLE `validation` (
+  `id` int(20) NOT NULL,
+  `token` varchar(230) NOT NULL,
+  `emp_id` int(11) NOT NULL,
+  `isactive` enum('true','false') NOT NULL DEFAULT 'false',
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -285,14 +348,15 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 ALTER TABLE `assignment`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `assignment_emp` (`employee`);
+  ADD KEY `assignment_emp` (`uploadby`);
 
 --
--- Indexes for table `auth_user`
+-- Indexes for table `authuser`
 --
-ALTER TABLE `auth_user`
-  ADD PRIMARY KEY (`emp_id`),
-  ADD KEY `auth_role` (`role_id`);
+ALTER TABLE `authuser`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `auth_emp` (`empid`),
+  ADD KEY `auth_role` (`roleid`);
 
 --
 -- Indexes for table `batch`
@@ -318,8 +382,8 @@ ALTER TABLE `department`
 --
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `emp_job` (`job`),
-  ADD KEY `emp_department` (`department`),
+  ADD KEY `emp_job` (`jobid`),
+  ADD KEY `emp_department` (`departmentid`),
   ADD KEY `emp_batch` (`batchclass`);
 
 --
@@ -365,6 +429,19 @@ ALTER TABLE `task`
   ADD KEY `task_student` (`student`);
 
 --
+-- Indexes for table `uplo`
+--
+ALTER TABLE `uplo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `validation`
+--
+ALTER TABLE `validation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `valid_emp` (`emp_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -375,34 +452,34 @@ ALTER TABLE `assignment`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `auth_user`
+-- AUTO_INCREMENT for table `authuser`
 --
-ALTER TABLE `auth_user`
-  MODIFY `emp_id` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `authuser`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `batch`
 --
 ALTER TABLE `batch`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `class`
 --
 ALTER TABLE `class`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `department`
 --
 ALTER TABLE `department`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `employees`
 --
 ALTER TABLE `employees`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `exam`
@@ -426,13 +503,25 @@ ALTER TABLE `grade_emp`
 -- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `uplo`
+--
+ALTER TABLE `uplo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `validation`
+--
+ALTER TABLE `validation`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -442,14 +531,14 @@ ALTER TABLE `task`
 -- Constraints for table `assignment`
 --
 ALTER TABLE `assignment`
-  ADD CONSTRAINT `assignment_emp` FOREIGN KEY (`employee`) REFERENCES `employees` (`id`);
+  ADD CONSTRAINT `assignment_emp` FOREIGN KEY (`uploadby`) REFERENCES `employees` (`id`);
 
 --
--- Constraints for table `auth_user`
+-- Constraints for table `authuser`
 --
-ALTER TABLE `auth_user`
-  ADD CONSTRAINT `auth_emp` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`id`),
-  ADD CONSTRAINT `auth_role` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`);
+ALTER TABLE `authuser`
+  ADD CONSTRAINT `auth_emp` FOREIGN KEY (`empid`) REFERENCES `employees` (`id`),
+  ADD CONSTRAINT `auth_role` FOREIGN KEY (`roleid`) REFERENCES `role` (`id`);
 
 --
 -- Constraints for table `batch`
@@ -462,8 +551,8 @@ ALTER TABLE `batch`
 --
 ALTER TABLE `employees`
   ADD CONSTRAINT `emp_batch` FOREIGN KEY (`batchclass`) REFERENCES `batch` (`id`),
-  ADD CONSTRAINT `emp_department` FOREIGN KEY (`department`) REFERENCES `department` (`id`),
-  ADD CONSTRAINT `emp_job` FOREIGN KEY (`job`) REFERENCES `job` (`id`);
+  ADD CONSTRAINT `emp_department` FOREIGN KEY (`departmentid`) REFERENCES `department` (`id`),
+  ADD CONSTRAINT `emp_job` FOREIGN KEY (`jobid`) REFERENCES `job` (`id`);
 
 --
 -- Constraints for table `exam`
@@ -485,6 +574,12 @@ ALTER TABLE `grade_emp`
 ALTER TABLE `task`
   ADD CONSTRAINT `task_assignment_cons` FOREIGN KEY (`assignment`) REFERENCES `assignment` (`id`),
   ADD CONSTRAINT `task_student` FOREIGN KEY (`student`) REFERENCES `employees` (`id`);
+
+--
+-- Constraints for table `validation`
+--
+ALTER TABLE `validation`
+  ADD CONSTRAINT `valid_emp` FOREIGN KEY (`emp_id`) REFERENCES `employees` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
